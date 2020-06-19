fc8b923d715d0daf2efa4533608f861774b73708
rayanepimentel <radharayg@gmail.com>
2020-06-19 17:46:18 -0300

    Aula 03 - Passando o type para a e b

--- projeto/index.ts
+++ projeto/index.ts
@@ -1,8 +1,11 @@
+
+//Precisamos dizer que o nosso input é tipo HTMLElement;
 const input1 = document.getElementById("num1") as HTMLElement;
 const input2 = document.getElementById("num2") as HTMLElement;
 const button = document.getElementById("button");
 
-function sum(a, b) {
+//passando o type
+function sum(a: number, b: number) {
     return a + b;
 }
 
