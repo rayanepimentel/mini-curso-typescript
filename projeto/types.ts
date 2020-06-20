e1befb9e4b469a1cc484616f1d2f2354c0a63f06
rayanepimentel <radharayg@gmail.com>
2020-06-20 16:55:37 -0300

    Aula 05 - Type boolean

---
+++ projeto/types.ts
@@ -0,0 +1,5 @@
+//type boolean (true//false)
+let isOpen: boolean
+isOpen = true //true é um booleano
+//isOpen = "true"
+//O tipo '"true"' não pode ser atribuído ao tipo 'boolean'. "true" é uma string
