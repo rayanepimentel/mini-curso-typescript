0ac71d0dbff825d5ae7cd7b823b5c9f8690b2531
rayanepimentel <radharayg@gmail.com>
2020-06-23 08:54:22 -0300

    Aula 10 - Interface extends

--- projeto/dist/interfaces.js
+++ projeto/dist/interfaces.js
@@ -4,7 +4,7 @@ var tlou = {
     title: 'The Last of Us',
     description: 'the best',
     genre: 'Action',
-    //plataform: ['PS3', 'PS4'],
+    plataform: ['PS3', 'PS4'],
     //agora podemos criar o método de fato
     //falamos que ele recebe title
     getSimilars: function (title) {
@@ -13,10 +13,20 @@ var tlou = {
     }
 };
 //chamar o método e passar o nome title
-tlou.getSimilars(tlou.title);
+if (tlou.getSimilars) {
+    tlou.getSimilars(tlou.title);
+}
 //podemos tbm chamar as propriedade
 console.log(tlou.title);
 //chamar Modifiers readonly(só para leitura)
 console.log(tlou.genre);
-//Posso modificar? Não
-//tlou.genre = 'New'
+//criar um objeto
+var leftbehind = {
+    //quando eu faço isso eu preciso passar as informações que o Game tem
+    title: 'The Last of leftbehind',
+    description: 'the best qualquer coisa',
+    genre: 'Action',
+    plataform: ['PS3', 'PS4'],
+    originalGame: tlou,
+    newContent: ['Qualquer coisa']
+};
--- projeto/interfaces.ts
+++ projeto/interfaces.ts
@@ -4,14 +4,14 @@ interface Game {
     readonly genre: string;
     plataform?: string[];
     //assinatura do método que vai receber title e vai retorna um void
-    getSimilars: (title: string) => void// o retorno (void) poderia ser number, array, qualquer coisa
+    getSimilars?: (title: string) => void// o retorno (void) poderia ser number, array, qualquer coisa
 }
 //objeto
 const tlou: Game = {
     title: 'The Last of Us',
     description: 'the best',
     genre: 'Action',
-    //plataform: ['PS3', 'PS4'],
+    plataform: ['PS3', 'PS4'],
     //agora podemos criar o método de fato
     //falamos que ele recebe title
     getSimilars: (title: string) => {
@@ -21,10 +21,33 @@ const tlou: Game = {
 }
 
 //chamar o método e passar o nome title
-tlou.getSimilars(tlou.title)
+if(tlou.getSimilars) {
+    tlou.getSimilars(tlou.title)
+}
 //podemos tbm chamar as propriedade
 console.log(tlou.title)
 //chamar Modifiers readonly(só para leitura)
 console.log(tlou.genre)
 //Posso modificar? Não
-//tlou.genre = 'New'
+//tlou.genre = 'New//
+
+
+//estender
+//quando fazemos isso, a Dlc tem todas as coisas do Game, mas as coisas que vamos definir para Dlc
+interface Dlc extends Game {
+    //a Dlc teria dentro dela um objeto do tipo Game, com todas as informações do jogo original
+    originalGame: Game
+    newContent: string[]
+}
+
+//criar um objeto
+
+const leftbehind: Dlc = {
+    //quando eu faço isso eu preciso passar as informações que o Game tem
+    title: 'The Last of leftbehind',
+    description: 'the best qualquer coisa',
+    genre: 'Action',
+    plataform: ['PS3', 'PS4'],
+    originalGame: tlou,
+    newContent: ['Qualquer coisa']
+}
